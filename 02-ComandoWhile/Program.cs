36f78ac596b676dfbd0b05858a6041062cdee454
Rafael Nunes <rafanunes82@yahoo.com.br>
2023-01-05 22:45:09 -0300

    Segunda Solucao - Aula 2

---
+++ 02-ComandoWhile/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _02_ComandoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i <= 10)
+            {
+                Console.WriteLine($"Contando: {i} x 5 = {i*5}");
+                i++; // i = i+1
+            }
+            Console.WriteLine("-----------------");
+
+            for (int j = 1; j <= 10; j++)
+            {
+                Console.WriteLine($"Contando: {j} x 5 = {j * 5}");
+            }
+           
+        }
+    }
+}
---
+++ Exe01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exe01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //quero cadastrar 2 alunos e suas 2 duas notas
+            //quero calcular a média destas notas
+            //se média maior 6 aprovado senão reprovado
+            string aluno1 = "Michael";
+            string aluno2 = "Andrea";
+
+            double a1n1 = 0;
+            double a1n2 = 0;
+            double a2n1 = 0;
+            double a2n2 = 0;
+
+            double ma1 = 0;
+            double ma2 = 0;
+
+            if (ma1>6) { } else { }
+
+            if (ma2<6) { } else { }
+
+            Console.WriteLine($"Media aluno: {aluno1} - {ma1}");
+            Console.WriteLine($"Media aluno: {aluno2} - {ma2}");
+        }
+    }
+}
